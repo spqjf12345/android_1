301d270cbed0bca9315501eb38389d9a2256cbfa
spqjf12345 <spqjf12345@gmail.com>
2020-12-30 17:17:19 +0900

    updated

--- app/src/main/java/com/example/android1/Fragment1.kt
+++ app/src/main/java/com/example/android1/Fragment1.kt
@@ -8,41 +8,59 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-
+import org.json.JSONObject
 
 
 class Fragment1 : Fragment() {
+    val list = ArrayList<list_item>()
+    lateinit var recyclerView1 : RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        val assetManager = resources.assets
+        val inputStream = assetManager.open("Contacts.json")
+        val jsonString = inputStream.bufferedReader().use{it.readText()}
+
+        val jObject = JSONObject(jsonString)
+
+        var jArray = jObject.getJSONArray("person")
+
+        for (i in 0 until jArray.length()) {
+            val obj = jArray.getJSONObject(i)
+            val name = obj.getString("name")
+            val number = obj.getString("number")
+            Log.d("TAG", "name($i): $name")
+            Log.d("TAG", "number($i): $number")
+            list.add(list_item(name, number))
+        }
+        /*
+        list.add(list_item("조소정", "010-3245-2959"))
+        list.add(list_item("이준용", "010-3245-2959"))
+        list.add(list_item("조소정", "010-3245-2959"))
+        list.add(list_item("이준용", "010-3245-2959"))
+         */
     }
+
 
     //여기에 그리기
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
+        //val mAdapter = contactAdapter(list)
+        //mRecyclerView.adapter = mAdapter
+
+        //val lm = LinearLayoutManager(this)
+        //mRecyclerView.layoutManager = lm
+        //mRecyclerView.setHasFixedSize(true)
+
+
         var rootView =  inflater.inflate(R.layout.fragment_a, container, false)
-        lateinit var recyclerView1 : RecyclerView
-
-        val list = ArrayList<list_item>()
-        var friend_DataArray: ArrayList<list_item> = ArrayList()
-
-        friend_DataArray.add(list_item("이름","닉"))
-        friend_DataArray.add(list_item("바보","닉"))
-        friend_DataArray.add(list_item("이름","닉"))
-
-        list.add(list_item("조소정", "010-3245-2959"))
-        list.add(list_item("이준용", "010-3245-2959"))
-        list.add(list_item("조소정", "010-3245-2959"))
-        list.add(list_item("이준용", "010-3245-2959"))
-
-
-
 
         recyclerView1 = rootView.findViewById(R.id.rv_json!!)as RecyclerView
         recyclerView1.layoutManager = LinearLayoutManager(this.context)
         recyclerView1.adapter = contactAdapter(list)
+        recyclerView1.setHasFixedSize(true)
 
         return rootView
     }
--- app/src/main/java/com/example/android1/contactAdapter.kt
+++ app/src/main/java/com/example/android1/contactAdapter.kt
@@ -5,9 +5,11 @@ import androidx.recyclerview.widget.RecyclerView
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import android.widget.Toast
 import kotlinx.android.synthetic.main.list_item.view.*
 import org.w3c.dom.Text
 import java.nio.file.Files.size
+import android.widget.Toast.makeText
 
 class contactAdapter(private val JsonList:ArrayList<list_item>):
         RecyclerView.Adapter<contactAdapter.ViewHolder>(){
@@ -19,7 +21,11 @@ class contactAdapter(private val JsonList:ArrayList<list_item>):
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): contactAdapter.ViewHolder {
             val inflatedView = LayoutInflater.from(parent.context).inflate(R.layout.list_item, parent, false)
-            return contactAdapter.ViewHolder(inflatedView)
+            return contactAdapter.ViewHolder(inflatedView).apply {
+                /*val curPos: Int = adapterPosition
+                var item: list_item = JsonList.get(curPos)
+                Toast.makeText(parent.context, "clicked ${item.name}\n ${item.number}", Toast.LENGTH_SHORT).show()*/
+            }
         }
 
 
