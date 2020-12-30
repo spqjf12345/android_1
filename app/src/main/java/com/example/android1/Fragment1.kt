e2aae33a8d121a068901eb69c5b924241a5178f6
starjrm00 <starjrm00@gmail.com>
2020-12-30 19:21:09 +0900

    Now we can add more information by click the button

--- app/src/main/java/com/example/android1/Fragment1.kt
+++ app/src/main/java/com/example/android1/Fragment1.kt
@@ -6,7 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.fragment_a.*
@@ -17,15 +18,23 @@ class Fragment1 : Fragment() {
     val list = ArrayList<list_item>()
     lateinit var recyclerView1 : RecyclerView
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        addButton.setOnClickListener{
+            val name =addName.text.toString()
+            val number = addNumber.text.toString()
+            if(name.isNotEmpty() && number.isNotEmpty()){
+                list.add(list_item(name, number))
+            }
+        }
+        refreshFragment(this, supportFragmentManager())
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val assetManager = resources.assets
         val inputStream = assetManager.open("Contacts.json")
         val jsonString = inputStream.bufferedReader().use{it.readText()}
-        private val addBtn : Button? = null
-
         val jObject = JSONObject(jsonString)
-
         var jArray = jObject.getJSONArray("person")
 
         for (i in 0 until jArray.length()) {
@@ -35,13 +44,6 @@ class Fragment1 : Fragment() {
             Log.d("TAG", "name($i): $name")
             Log.d("TAG", "number($i): $number")
             list.add(list_item(name, number))
-        }
-        addButton.setOnClickListener{
-            val name =addName.text.toString()
-            val number = addNumber.text.toString()
-            if(name.length != 0 && number.length != 0){
-                list.add(list_item(name, number))
-            }
         }
     }
 
@@ -62,5 +64,9 @@ class Fragment1 : Fragment() {
         return rootView
     }
 
+    fun refreshFragment(fragment: Fragment, fragmentManager: FragmentManager){
+        var ft: FragmentTransaction = fragmentManager.beginTransaction()
+        ft.detach(fragment).attach(fragment).commit()
+    }
 
 }
